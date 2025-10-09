360a60af91534f3f8e67bd82f5812ec3cc9802ba
Durdona Yuldasheva <durdonayuldasheva989@gmail.com>
2025-10-09 21:12:52 +0500

    Museum Artifact Label

---
+++ museum_label.py
@@ -0,0 +1,20 @@
+name = "Mona Lisa"
+period = "Italian Renaissance"
+country = "Italy"
+year = "1797"
+location = "Denon Wing"
+
+print("--------------------------------------------------")
+print("      THE NATIONAL MUSEUM OF HISTORY")
+print("--------------------------------------------------")
+print()
+print("Artifact:      " + name)
+print("Origin:        " + country)
+print("Period:        " + period)
+print("Acquired:      " + year)
+print()
+print("Catalog Number: " + 'ACQ-' + year + '-' + country)
+print()
+print("--------------------------------------------------")
+print("Location: "+ location)
+print("--------------------------------------------------")
